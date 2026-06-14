3e54af5c00213441dfef10374763df04ba5cdf0f
Atsushi Shimono <atsushi@himor.in>
2026-06-15 01:08:59 +0900

    fix

--- computers/labelprint.pl
+++ computers/labelprint.pl
@@ -16,13 +16,14 @@ close(INDAT);
 my $params = decode_json($config_text);
 
 $o_spl->init_page(256, 392, "shiftjis");
-$o_spl->print_xfont(25, 225, 1, 1, 22, "surface7b");
-$o_spl->print_ascii(60, 230, 1, 1, "M", "Ci5-1135G7 2.40GHz");
-$o_spl->print_ascii(85, 230, 1, 1, "M", "Mem 8G / SSD 512GB");
-$o_spl->print_ascii(110, 230, 1, 1, "M", "Surface Pro 7+ 040718110253");
-$o_spl->print_ascii(135, 230, 1, 1, "M", "Win11 Pro / Norm");
-$o_spl->print_ascii(160, 230, 1, 1, "M", "atsushi");
-$o_spl->print_ascii(185, 230, 1, 1, "M", "O365, AdobeCC");
+$o_spl->print_xfont(25, 225, 1, 1, 23, "surface7b");
+$o_spl->print_ascii(80, 230, 1, 1, "M", "Core i5-1135G7 2.40GHz");
+$o_spl->print_ascii(105, 230, 1, 1, "M", "Mem 8G / SSD 512GB");
+$o_spl->print_ascii(130, 230, 1, 1, "M", "Surface Pro 7+ 040718110253");
+$o_spl->print_ascii(155, 230, 1, 1, "M", "Win11 Pro / Norm");
+$o_spl->print_ascii(180, 230, 1, 1, "M", "atsushi");
+$o_spl->print_ascii(205, 230, 1, 1, "M", "O365, AdobeCC");
+$o_spl->print_ascii(230, 230, 1, 1, "M", "O365, AdobeCC");
 $o_spl->print_qr(150, 505, "Q", 3, 'https://himor.in/computers/?surface7b');
 $o_spl->finish_page(1);
 
